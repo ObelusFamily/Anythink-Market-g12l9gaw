d300b02db529c8ce4b145a33a2657dba73b80115
urielwix <109895228+urielwix@users.noreply.github.com>
2023-02-11 12:27:12 +0000

    add api key

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -4,6 +4,7 @@ var Item = mongoose.model("Item");
 var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
+var openai = require("openai");
 const { sendEvent } = require("../../lib/event");
 
 // Preload item objects on routes with ':item'
@@ -147,7 +148,7 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
       if (!item.image){
         openai.createImage({
-          prompt: "a white siamese cat",
+          prompt: item.title,
           n: 1,
           size: "1024x1024",
         }).then(response => item.image= response.data.data[0].url);
